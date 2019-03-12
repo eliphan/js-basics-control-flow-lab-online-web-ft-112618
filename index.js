7e01224d2796408d97bdc8735707323472861ef2
Elise Phan <aliceconnic@gmail.com>
2019-03-12 20:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     result = 'This one is on me!'
   }
-  else if (someValue > 2000) {
+  else if (someValue > 2000 && someValue < 2500) {
     result = 'I will gladly take your thirty bucks.'
   }
+  else
   return result
 }
