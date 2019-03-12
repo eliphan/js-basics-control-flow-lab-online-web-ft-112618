cce1480f95c670c6ed7fe06496504c134c9a8ea5
Elise Phan <aliceconnic@gmail.com>
2019-03-12 20:15:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
   let result
-  if (someValue < 30) {
+  if (someValue <= 400) {
     result = 'This one is on me!'
   }
   else if (someValue > 2000) {
