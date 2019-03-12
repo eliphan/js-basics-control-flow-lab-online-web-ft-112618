55fb9a7c0f8ec6d811caf61711a41078c737d966
Elise Phan <aliceconnic@gmail.com>
2019-03-12 20:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
   let result
-  if (distance > 2000) {
+  if (someValue > 2000) {
     result = 30
   }
   return result
